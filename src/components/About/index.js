798faaaa4d683f1b169090c909adf23915c2230a
Hilary Lewis <hilarylewis92@gmail.com>
2016-12-29 14:52:11 -0700

    Styles about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -46,15 +46,21 @@ export default class About extends Component {
         <div className="header">
           <Header />
         </div>
-        <section className='parallax'>
+
+        <section className='parallax-image'>
         </section>
+
         <img
           className='hilary-lewis-image'
           src={require ('./hilary-lewis.jpg')}
           role='none'
         />
-      <h3 className='title'>About Me</h3>
-      <section className='about'>
+
+      <section className='about-me'>
+        <h3 className='title'>
+          About Me
+        </h3>
+
         <section className="summary">
           <p className="summary-paragraph">
             As a creative-minded front-end developer, I value using both my left and right brain. I enjoy using my technical skills to solve challenging problems and write efficient code while also using my creative side to design clean and unique user interface on applications and websites.
@@ -64,28 +70,50 @@ export default class About extends Component {
             As a lifelong learner, I am always looking to add useful skills. Whether it's becoming a web developer, math teacher, yoga instructor, or coaching soccer, learning is one of my most rewarding passions.
           </p>
         </section>
+
         <section className='experience-list'>
-          <h3 className='experience'>EXPERIENCE</h3>
+          <h3 className='experience-title'>
+            EXPERIENCE
+          </h3>
+
           {this.state.experience.map(item => {
             return(
               <div className='single-experience'>
-                <h5 className='position'>{item.position}</h5>
-                <h6 className='company'>{item.company}</h6>
-                <p className='dates'>{item.dates}</p>
+                <h5 className='position'>
+                  {item.position}
+                </h5>
+                <h6 className='company'>
+                  {item.company}
+                </h6>
+                <p className='dates'>
+                  {item.dates}
+                </p>
               </div>
             )
           })}
         </section>
+
         <section className="skills-list">
-          <h3 className='skills'>SKILLS</h3>
+          <h3 className='skills-title'>
+            SKILLS
+          </h3>
+
           {this.state.skills.map(item => {
             return(
-              <h5 className='single-skill'>{item}</h5>
+              <h5 className='single-skill'>
+                {item}
+              </h5>
             )
           })}
         </section>
 
-        <Link to='/resume' activeClassName='active' className='resume-link'>View My Resume</Link>
+        <section className='resume-section'>
+          <Link to='/resume'
+            activeClassName='active'
+            className='resume-link'>
+            View My Resume
+          </Link>
+        </section>
       </section>
       </div>
     );
