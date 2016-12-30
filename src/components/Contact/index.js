1855722db280435edc51f5b95c9dd80a909cbf4c
Hilary Lewis <hilarylewis92@gmail.com>
2016-12-30 14:21:30 -0700

    Adds contact me page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,14 +8,29 @@ import './style.css';
 export default class Contact extends Component {
   render() {
     return (
-      <div>
+      <div className='Contact'>
         <div className="header">
           <Header />
         </div>
 
-        <h1>
-          Contact
-        </h1>
+        <section className='contact-section'>
+          <h1 className='contact-title'>
+            Get in Touch
+          </h1>
+
+          <p className='contact-summary'>
+            The best way to get in touch with me is to send me an email. I will try my best to respond in less than 24 hours.
+          </p>
+          <br />
+          <a href='mailto:hilarylewis92@gmail.com' className='contact-email'>
+            hilarylewis92@gmail.com
+          </a>
+          <br />
+          <br />
+          <p>
+            You can also reach me though social media below.
+          </p>
+        </section>
 
         <section className='footer-section'>
           <Footer />
