5ecc023aea3398bb9b622e880a30dbe8ab1de995
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-02 11:32:02 -0700

    Add hamburger menu but not responsive with react

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,21 +4,51 @@ import { Link } from 'react-router'
 import './style.css';
 
 export default class Header extends Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     open: false;
+  //   }
+  //
+  //   handleClick() {
+  //     this.setState({
+  //         open: !this.state.open
+  //     })
+  //   }
+
+
+
   render() {
     return (
       <div role="nav" className='Header'>
-        <Link to="/" activeClassName="active" className='link'>
-          Hilary
-        </Link>
-        <Link to="/contact" activeClassName="active" className='link right'>
-          Contact
-        </Link>
-        <Link to="/about" activeClassName="active" className='link middle-nav right'>
-          About
-        </Link>
-        <Link to="/projects" activeClassName="active" className='link right'>
-           Projects
-        </Link>
+        <div className='hamburger-cross'>
+          <button className="hamburger">
+            &#9776;
+          </button>
+          <button className="cross">
+            &#735;
+          </button>
+        </div>
+
+        <div className='menu'>
+          <ul>
+            <Link to="/" activeClassName="active" className='link'>
+              Hilary
+            </Link>
+
+            <Link to="/projects" activeClassName="active" className='link right'>
+              <li>Projects</li>
+            </Link>
+
+            <Link to="/about" activeClassName="active" className='link middle-nav right'>
+              <li>About</li>
+            </Link>
+
+            <Link to="/contact" activeClassName="active" className='link right'>
+              <li>Contact</li>
+            </Link>
+          </ul>
+        </div>
       </div>
     );
   }
