de10d019f33587406c0e6b17fa297d927b31dd99
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-02 20:14:48 -0700

    Move recent state into home component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,93 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router'
-import { map } from 'lodash';
+import { map } from 'lodash'
 import { TweenMax } from 'gsap'
+import { Loader } from 'react-loader'
 
-import './style.css';
+import './style.css'
 import Header from '../Header'
 import About from '../About'
 import Projects from '../Projects'
 import Contact from '../Contact'
 import Footer from '../Footer'
+import Home from './Home'
 
 export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      recent: [
-        {url: 'https://nimblenetwork-d13c3.firebaseapp.com/',
-        src: './images/nimble-network.png',
-        title: 'Nimble Network'},
-        {url: 'https://shoot-the-breeze-1be68.firebaseapp.com/',
-        src: './images/shoot-the-breeze.png',
-        title: 'Shoot the Breeze'},
-        {url: 'https://hilarylewis92.github.io/weather-forecast/#/?_k=myzv4x',
-        src: './images/weather-dux.png',
-        title: 'Weather Dux'}
-      ]
-    }
-  }
-
-  componentDidMount() {
-    this.gsapHeroAnimation()
-  }
-
-  gsapHeroAnimation() {
-    TweenMax.to('.hero-text-landing', 1.5,
-    {bottom: 220}
-    )
-  }
-
   render() {
     return (
       <div className='App'>
         <Header />
-        <section className='hero-background-image'>
-          <section className='hero-text-landing'>
-            HILARY LEWIS
-            <br />
-            Front-End Developer
-            <br />
-            Denver, Colorado
-          </section>
-        </section>
 
-        <section className='recent-projects-section'>
-          <h3 className='recent-projects-title'>
-            Recent Projects
-          </h3>
+        <Home />
 
-          <section className='recent-projects-list'>
-            {this.state.recent.map(project => {
-              return(
-                <div className='single-project'>
-                  <a href={project.url}>
-                    <h4 className='project-title'>
-                      {project.title}
-                    </h4>
-
-                    <img
-                      className='project-image'
-                      src={require(project.src)}
-                      role='none'
-                    />
-                  </a>
-                </div>
-              )
-            })}
-          </section>
-
-          <section className='view-more-section'>
-            <Link to='/projects'
-              className='view-more-projects button'>
-              View More Projects &rarr;
-            </Link>
-          </section>
-        </section>
-
-        <section className='footer-section'>
-          <Footer />
-        </section>
+        <Footer />
       </div>
     );
   }
